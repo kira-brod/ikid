2a5bc8bdd311e7a8ed5db499925b38b3a822f8a5
Kira Brodsky <kbrods@uw.edu>
2025-04-25 13:08:32 -0700

    Initial Commit

---
+++ ikidTests/ikidTests.swift
@@ -0,0 +1,17 @@
+//
+//  ikidTests.swift
+//  ikidTests
+//
+//  Created by Kira Brodsky on 4/25/25.
+//
+
+import Testing
+@testable import ikid
+
+struct ikidTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
